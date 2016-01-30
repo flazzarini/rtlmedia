3b4c941621773341f3aae0e35ffa877a9a4e301c
Frank Lazzarini <flazzarini@gmail.com>
2016-01-30 17:51:31 +0100

    Added simple setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os.path import join
+
+NAME = "rtljournalrss"
+DESCRIPTION = "API which creates structure around RTL.lu Media Gallery"
+AUTHOR = "Frank Lazzarini"
+AUTHOR_EMAIL = "flazzarini@gmail.com"
+VERSION = open(join(NAME, 'version.txt')).read().strip()
+LONG_DESCRIPTION = open("README.rst").read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="Private",
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'bottle',
+    ],
+    entry_points={
+        'console_scripts': []
+    },
+    dependency_links=[],
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    zip_safe=False,
+)
